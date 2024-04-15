3df32eee59eaa389a54d9de1368413835e513d34
Rafael Carneiro Pregardier <rafaelpregardier@gmail.com>
2024-04-15 15:36:23 -0300

    add java

--- TRABALHO 1/leitor_csv.py
+++ TRABALHO 1/leitor_csv.py
@@ -3,12 +3,21 @@ import pandas as pd
 import json
 
 current_dir = os.path.dirname(__file__)
-csv_file = os.path.join(current_dir, 'QueryResults_python_442.csv')
+csv_file_python = os.path.join(current_dir, 'QueryResults_python_442.csv')
 
-df = pd.read_csv(csv_file)
+df = pd.read_csv(csv_file_python)
 
 for index, row in df.iterrows():
     if 'Title' in row and pd.notnull(row['Title']) and 'python' in row['Title'].lower():
-        print(row['Title'])
-        with open('python_issues.json', 'a') as f:
+        with open('python_questions.json', 'a') as f:
+            f.write(json.dumps(row.to_dict()) + '\n')
+            
+            
+csv_file_java = os.path.join(current_dir, 'QueryResults_java_182.csv')
+
+df = pd.read_csv(csv_file_java)
+
+for index, row in df.iterrows():
+    if 'Title' in row and pd.notnull(row['Title']) and 'java' in row['Title'].lower():
+        with open('java_questions.json', 'a') as f:
             f.write(json.dumps(row.to_dict()) + '\n')
